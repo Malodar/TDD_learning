fb55a289dda1f14cb36871ec7022b67b96d79c04
Artsiom Kunitsyn <kunitsyn-a-v@yandex.ru>
2019-06-11 13:35:38 +0300

    FT now tests form filling on the page

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,9 @@
 # -*- coding: UTF-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 import unittest
 
 
@@ -8,6 +12,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox(executable_path='/home/artsiom/geckodriver')
+        self.browser.wait = WebDriverWait(self.browser, 15)
 
     def tearDown(self):
         """демонтаж"""
@@ -22,18 +27,30 @@ class NewVisitorTest(unittest.TestCase):
 
         """Она видит, что заголовок и шапка страницы говорят о списках неотложных дел"""
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         """Ей сразу же предлагается ввести элемент списка"""
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         """Она набирает в текстовом поле "купить павлиньи перья" (ее хобби - 
         вязание рыболовных мушек)"""
+        inputbox.send_keys('купить павлиньи перья')
 
         """Когда она нажимает Enter, страница обновляется, и теперь страница содержит 
         "1: Купить павлиньи перья" в качестве элемента списка"""
+        inputbox.send_keys(Keys.ENTER)
+        self.browser.wait.until(EC.presence_of_all_elements_located)
+        sleep(0.2)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1. купить павлиньи перья' for row in rows))
 
         """Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)"""
+        self.fail('Закончить тест!')
 
         """Страница снова обновляется, и теперь показывает оба элемента ее списка"""
 
