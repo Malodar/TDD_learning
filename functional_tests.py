5542fbc233e732e94dcae7b47facef0ee0551349
Artsiom Kunitsyn <kunitsyn-a-v@yandex.ru>
2019-06-11 15:18:03 +0300

    asdasd

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,12 @@ class NewVisitorTest(unittest.TestCase):
         """демонтаж"""
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Подтверждение строки в таблице списка"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
 
@@ -42,23 +48,28 @@ class NewVisitorTest(unittest.TestCase):
         "1: Купить павлиньи перья" в качестве элемента списка"""
         inputbox.send_keys(Keys.ENTER)
         self.browser.wait.until(EC.presence_of_all_elements_located)
-        sleep(0.2)
+        sleep(1.2)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1. купить павлиньи перья', [row.text for row in rows]),
-
+        self.check_for_row_in_list_table('1: купить павлиньи перья')
 
         """Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)"""
-        self.fail('Закончить тест!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        self.browser.wait.until(EC.presence_of_all_elements_located)
+        sleep(1.2)
+
+
 
         """Страница снова обновляется, и теперь показывает оба элемента ее списка"""
+        self.check_for_row_in_list_table('1: купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
 
         """Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         сайт сгенерировал для нее уникальный URL-адрес -- об этом выводится небольшой текст 
         с обьяснениями"""
-
+        self.fail('Закончить тест!')
         """Она посещает этот URL-адрес -- ее список по-прежнему там."""
 
         """Удовлетворенная, она снова ложится спать"""
