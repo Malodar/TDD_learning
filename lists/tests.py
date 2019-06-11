022b68db37285a310e9d65b382fc41e512f810ca
Artsiom Kunitsyn <kunitsyn-a-v@yandex.ru>
2019-06-11 12:38:03 +0300

    First module test and url resolve, fictive view

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import home_page
 
 # Create your tests here.
 
 
 class SmokeTest(TestCase):
-    """тест на токсичность"""
+    """тест домашней страницы"""
 
-    def test_bad_maths(self):
-        """тест: неправильные математические расчеты"""
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolvers_to_home_page_view(self):
+        """тест: корневой URL преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
